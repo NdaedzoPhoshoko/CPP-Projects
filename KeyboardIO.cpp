ee60379573afd9320c33fb7bdb3d865808a5a106
Ndaedzo Phoshoko <121657564+NdaedzoPhoshoko@users.noreply.github.com>
2023-05-26 11:20:15 +0200

    Create KeyboardIO.cpp
    The project not stored in a folder form to configure environment. Though content can have topic: How to use Input and Output streams in CZ++

---
+++ KeyboardIO.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//function to computer Accumulated finance ammount 
+void MyFunc(int P, int m, float i )
+{
+    int A = pow( (1 + (i / m)) , t * m);
+    int c = P * A;
+cout<<" ANSWER: "<< c <<endl; //text output
+}
+
+int main() //ENGINE
+{
+  //variable declarations
+   int P, m;
+   float i;
+  //collecting user input
+   cout<<" Enter the principal amount: ";
+   cin>> P;
+   cout<<"\n Your principal amount is compounded for 1, 2 or 4 times per year?: ";
+   cin>> m;
+   cout << "\n Enter interest in decimal form : ";
+   cin>> i;
+
+   MyFunc(P,t,m,i); //calling user-defined functions
+
+ return 0;
+}
